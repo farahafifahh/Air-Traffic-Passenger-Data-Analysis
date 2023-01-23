bbcc2ec44a5b1f71496539e6051f0a6178161dab
farahafifahh <farahafifah@student.usm.my>
2023-01-23 16:57:39 +0800

    Read CSV

---
+++ Code.R
@@ -0,0 +1,8 @@
+# Set the working directory
+setwd("/Users/farahafifah/R Programming/Air Traffic Passenger Data/Air Traffic Passenger Data Analysis")
+
+# Read a CSV files using read.csv() function
+data <- read.csv("Air_Traffic_Passenger_Data.csv")
+
+# Print the summary of the imported data
+summary(data)
